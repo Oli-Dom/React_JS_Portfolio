5314019ae3f756528522325123ad2f306f730a23
Oli-Dom <oadl12345678@gmail.com>
2023-03-09 00:49:47 -0500

    Started the project

---
+++ reactPortfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        Hi, My name is Oliver Dominguez. CS Major @ Lehman College and tech
+        enthusiast.
+      </main>
+    </div>
+  );
+}
+
+export default App;
